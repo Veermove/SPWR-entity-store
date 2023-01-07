d3967371d3ac4def497ea6f29f480dbe1f077251
Veermove <veermove@users.noreplay.github.com>
2023-01-07 17:21:51 +0100

    Init

---
+++ src/config/routes.ts
@@ -0,0 +1,37 @@
+/**
+ * Routes configuration.
+ *
+ * @module pwrspotted-app
+ * @license BSD-2-Clause
+ * @copyright Mat. 2020
+ */
+
+
+
+
+import { useMemory } from "../index";
+import { apiV1 } from "./env";
+
+import { hello } from "../actions/hello";
+
+
+
+
+/**
+ * Routes configuration.
+ */
+export default function configureRoutes (): void {
+
+    const { app } = useMemory();
+
+    // "hello world" route
+    app.get(`${apiV1}/`, hello);
+
+
+    // // "create user" route
+    // app.post(`${apiV1}/user/create`, createUser);
+
+    // // "isAdmin user" route
+    // app.post(`${apiV1}/user/isAdmin`, isAdmin);
+
+}
