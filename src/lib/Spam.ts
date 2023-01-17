4f4f3cbafbef4571c8f7b96591da749fb9107502
Veermove <veermove@users.noreplay.github.com>
2023-01-17 14:00:30 +0100

    Fix for spam filtering poll options

--- src/lib/Spam.ts
+++ src/lib/Spam.ts
@@ -7,14 +7,15 @@ const MAX_NUMBER_OF_CURSE_WORDS = 10;
 export const isMessageSpam = (
     content: string,
     tags?: string[],
-    pollOptions?: string[],
+    pollOptions?: { option: string, votes: number}[],
 ) => {
 
     const
         sTags = tags ? tags : [],
         sPollOps = pollOptions ? pollOptions : [],
+        mappedOps = sPollOps.map(s => s.option),
         messageWords = new Set(
-            [content, ...sTags, ...sPollOps]
+            [content, ...sTags, ...mappedOps]
                 .flatMap((s) => s.split(" "))
                 .map((s) => s.toLowerCase().replaceAll("#", "")),
         );
